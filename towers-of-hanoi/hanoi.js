4cdbe4435dd042a860dd729befe6888e99fa0bbe
Andre Nanninga <ag.nanninga@gmail.com>
2014-09-22 15:17:38 +0300

    polish and gui

--- towers-of-hanoi/hanoi.js
+++ towers-of-hanoi/hanoi.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var Hanoi = function(disks) {
-  this.reset(disks);
+var Hanoi = function(discs) {
+  this.reset(discs);
 };
 
-Hanoi.prototype.reset = function(disks) {
-  this.disks = disks;
-  this.moves = 0;
+Hanoi.prototype.reset = function(discs, algorithm) {
+  this.discs = discs;
+  this.states = [];
+  this.algorithm = algorithm || 'recursive';
 
   this.towers = {
     'A': [],  // initial 
@@ -14,58 +15,207 @@ Hanoi.prototype.reset = function(disks) {
     'C': []  // target
   };
 
-  for(var i = this.disks; i > 0; i--) {
+  for(var i = this.discs; i > 0; i--) {
     this.towers.A.push(i);
   }
 };
 
-Hanoi.prototype.solveRecursive = function() {
-  console.log('Initial state\n');
-  this.print();
+Hanoi.prototype.saveState = function() {
+  this.states.push([
+    this.towers.A.slice(0),
+    this.towers.B.slice(0),
+    this.towers.C.slice(0)
+  ]);
+};
 
-  this._solveRecursive('A', 'B', 'C', this.disks);
+Hanoi.prototype.replay = function() {
+  var self = this;
+  var stdin = process.openStdin();
+  
+  console.log('press any key to show the ' + this.algorithm + ' solution');
+
+  stdin.on('data', function() {
+    var state = self.states.splice(0, 1)[0];
+
+    process.stdout.write('\u001B[2J\u001B[0;0f');
+
+    if(self.algorithm === 'recursive') {
+      self.print('linear', state);
+    }
+    else if(self.algorithm === 'iterative') {
+      self.print('circular', state);
+    }
+
+    if(self.states.length === 0) {
+      process.exit(0);
+    }
+
+    return false;
+  });
+};
+
+Hanoi.prototype.solve = function(algorithm) {
+  if(algorithm === 'recursive') {
+    this.solveRecursive();
+  }
+  else if(algorithm === 'iterative') {
+    this.solveIterative();
+  }
+
+  this.replay();
+};
+
+Hanoi.prototype.solveRecursive = function() {
+  this.algorithm = 'recursive';
+  this.saveState();
+
+  this._solveRecursive('A', 'B', 'C', this.discs);
 };
 
 Hanoi.prototype._solveRecursive = function(x, y, z, n) {
   if(n > 0) {
     this._solveRecursive(x, z, y, n - 1);
 
-    // move disk from tower x to tower z
-    var disc = this.towers[x].indexOf(n); // find index of the disc
-    this.towers[x].splice(disc, 1); // remove disc from tower
-    this.towers[z].push(n); // add disc to other tower
-
-    this.moves += 1;
-    console.log('Move ' + this.moves + ': move disk ' + n + ' from tower \'' + x + '\' to tower \'' + z + '\'\n');
-
-    this.print();
+    // move disc from tower x to tower z
+    this.moveDisc(n, this.towers[x], this.towers[z]);
+    this.saveState();
 
     this._solveRecursive(y, x, z, n - 1);
   }
 };
 
 Hanoi.prototype.solveIterative = function() {
+  this.algorithm = 'iterative';
+  this.saveState();
+
+  var lastDisc = -1;
+  var self = this;
+
+  var isTopDiscOnTower = function(tower, disc) {
+    return tower.indexOf(disc) === tower.length - 1;
+  };
+
+  var getNextTower = function(tower, direction) {
+    var towers = ['A', 'B', 'C'];
+    var index = towers.indexOf(tower);
+
+    index += direction;
+
+    if(index >= towers.length) {
+      index = 0;
+    }
+    else if(index < 0) {
+      index = towers.length - 1;
+    }
+
+    return self.towers[towers[index]];
+  };
+
+  var isLegalMove = function(disc, fromTower, toTower) {
+    var index = fromTower.indexOf(disc); // find index of the disc
+
+    if(index === -1) {
+      return false;
+    }
+
+    if(index !== fromTower.length - 1) {
+      return false;
+    }
+
+    if(toTower[toTower.length - 1] < disc) {
+      return false;
+    }
+
+    return true;
+  };
+
+  while(this.towers.B.length !== this.discs && this.towers.C.length !== this.discs) {
+    var madeMove = false;
+    for(var i = 1; i <= this.discs; i++) {
+
+      if(i === lastDisc) {
+        continue;
+      }
+
+      var direction = i % 2 === 0 ? -1 : 1;
+      var towers = ['A', 'B', 'C'];
+
+      for(var j = 0; j < towers.length; j++) {
+        var tower = this.towers[towers[j]];
+
+        if(isTopDiscOnTower(tower, i)) {
+          var nextTower = getNextTower(towers[j], direction);
+          
+          if(isLegalMove(i, tower, nextTower)) {
+            this.moveDisc(i, tower, nextTower);
+            lastDisc = i;
+            madeMove = true;
+
+            this.saveState();
+            break;
+          }
+        }
+      }
+
+      if(madeMove) {
+        break;
+      }
+    }
+  }
 };
 
-Hanoi.prototype.print = function() {
-  // print each tower
-  var towers = [
-    this._towerToString(this.towers.A, 'A').split('\n'),
-    this._towerToString(this.towers.B, 'B').split('\n'),
-    this._towerToString(this.towers.C, 'C').split('\n')
-  ];
+Hanoi.prototype.moveDisc = function(disc, fromTower, toTower) {
+  var index = fromTower.indexOf(disc); // find index of the disc
+  fromTower.splice(index, 1); // remove disc from tower
+  toTower.push(disc); // add disc to other tower
+};
+
+Hanoi.prototype.print = function(layout, state) {
+  var canvas = [];
+  var i;
+  var towers;
+  layout = layout || 'linear';
+
+  if(state) {
+    // print each tower
+    towers = [
+      this._towerToString(state[0], 'A').split('\n'),
+      this._towerToString(state[1], 'B').split('\n'),
+      this._towerToString(state[2], 'C').split('\n')
+    ];
+  }
+  else {
+    // print each tower
+    towers = [
+      this._towerToString(this.towers.A, 'A').split('\n'),
+      this._towerToString(this.towers.B, 'B').split('\n'),
+      this._towerToString(this.towers.C, 'C').split('\n')
+    ];
+  }
 
   // join the lines for each tower together to create a side-by-side view
-  var canvas = [];
-  for(var i = 0; i < towers[0].length; i++) {
-    canvas.push(towers[0][i] + ' ' + towers[1][i] + ' ' + towers[2][i]);
+  if(layout === 'linear') {
+    for(i = 0; i < towers[0].length; i++) {
+      canvas.push(towers[0][i] + ' ' + towers[1][i] + ' ' + towers[2][i]);
+    }
+  }
+  else if(layout === 'circular') {
+    var padding = new Array(Math.round(towers[0].length / 2) + 1).join(' ');
+
+    for(i = 0; i < towers[0].length; i++) {
+      canvas.push(towers[0][i] + ' ' + towers[1][i]);
+    }
+
+    for(i = 0; i < towers[0].length; i++) {
+      canvas.push(padding + towers[2][i]);
+    }
   }
 
   console.log(canvas.join('\n') + '\n');
 };
 
 Hanoi.prototype._towerToString = function(tower, name) {
-  var width = this.disks * 2 + 3;
+  var width = this.discs * 2 + 3;
   var middle = Math.floor(width / 2);
   name = name || ' ';
 
@@ -80,21 +230,21 @@ Hanoi.prototype._towerToString = function(tower, name) {
   var canvas = [];
 
 
-  // check for the maximum amount of disk possible on a single tower
-  for(var i = this.disks; i >= 0; i--) {
-    // if a disk is found at the index then print it
+  // check for the maximum amount of disc possible on a single tower
+  for(var i = this.discs; i >= 0; i--) {
+    // if a disc is found at the index then print it
     if(tower[i]) {
-      var disk = tower[i];
-      var diskWidth = disk * 2 + 1;
-      var outerPadding = Math.floor((width - diskWidth) / 2);
-      var innerPadding = disk - 1;
+      var disc = tower[i];
+      var discWidth = disc * 2 + 1;
+      var outerPadding = Math.floor((width - discWidth) / 2);
+      var innerPadding = disc - 1;
 
-      // create a line for the top of a disk
-      canvas.push(repeat(outerPadding) + repeat(diskWidth, '-') + repeat(outerPadding));
-      // create the body for the disk
+      // create a line for the top of a disc
+      canvas.push(repeat(outerPadding) + repeat(discWidth, '-') + repeat(outerPadding));
+      // create the body for the disc
       canvas.push(repeat(outerPadding) + '|' + repeat(innerPadding) + tower[i] + repeat(innerPadding) + '|' + repeat(outerPadding));
     }
-    // if no disk is found then print the stick
+    // if no disc is found then print the stick
     else {
       canvas.push(repeat(middle) + '|' + repeat(middle));
       canvas.push(repeat(middle) + '|' + repeat(middle));
--- towers-of-hanoi/index.js
+++ towers-of-hanoi/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
 var Hanoi = require('./hanoi');
+var opts = require('nomnom')
+  .option('discs', {
+    abbr: 'd',
+    help: 'The total amount of discs used for the puzzle',
+    required: true
+  })
+  .option('algorithm', {
+    abbr: 'a',
+    default: 'recursive',
+    choices: ['recursive', 'iterative']
+  })
+  .parse();
 
-var hanoi = new Hanoi(5);
-hanoi.solveRecursive();
+var hanoi = new Hanoi(opts.discs);
+hanoi.solve(opts.algorithm);
